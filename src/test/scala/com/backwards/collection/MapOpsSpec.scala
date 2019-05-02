e042d561a22128d73a889c86c6d3c8ba72889a55
David Ainslie <dainslie@gmail.com>
2019-05-02 22:13:00 +0100

    Map ops

--- src/test/scala/com/backwards/collection/MapOpsSpec.scala
+++ src/test/scala/com/backwards/collection/MapOpsSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{MustMatchers, WordSpec}
 class MapOpsSpec extends WordSpec with MustMatchers with MapOps {
   "Map ops" should {
     "convert Map to Properties" in {
-      """val p: Properties = Map[String, Int]("one" -> 1, "two" -> 2)""" must compile
+      """val p: java.util.Properties = Map[String, Int]("one" -> 1, "two" -> 2)""" must compile
     }
   }
 }
