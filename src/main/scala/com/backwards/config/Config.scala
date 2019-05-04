a7a656c94d2ce021bb590a83d3cfeff56e1f4a40
David Ainslie <dainslie@gmail.com>
2019-05-04 16:11:11 +0100

    Classpath config resource

--- src/main/scala/com/backwards/config/Config.scala
+++ src/main/scala/com/backwards/config/Config.scala
@@ -7,7 +7,7 @@ import scala.reflect.ClassTag
 import io.lemonlabs.uri.Uri
 import pureconfig._
 import pureconfig.generic.ExportMacros
-import com.typesafe.config.{Config => TypesafeConfig}
+import com.typesafe.config.{ConfigFactory, Config => TypesafeConfig}
 
 object Config extends Config
 
@@ -15,6 +15,8 @@ object Config extends Config
   * This trait, for convenience, hard codes the use of automatic derivation of readers and writers via copied code from [[pureconfig.generic.auto]]
   */
 trait Config {
+  type Classpath = String
+
   implicit def exportReader[A]: Exported[ConfigReader[A]] = macro ExportMacros.exportDerivedReader[A]
   implicit def exportWriter[A]: Exported[ConfigWriter[A]] = macro ExportMacros.exportDerivedWriter[A]
 
@@ -25,6 +27,11 @@ trait Config {
 
   def load[C: ClassTag](namespace: String)(implicit READER: Derivation[ConfigReader[C]]): C =
     loadConfigOrThrow[C](namespace)
+
+  def load[C: ClassTag](classpath: Classpath, namespace: String)(implicit READER: Derivation[ConfigReader[C]]): C =
+    loadConfigOrThrow[C](ConfigFactory.load(classpath), namespace)
+
+  // TODO - Not sure I want the rest...
 
   def load[C: ClassTag](config: TypesafeConfig, namespace: String)(implicit READER: Derivation[ConfigReader[C]]): C =
     loadConfigOrThrow[C](config, namespace)
