6921584f364f13fd5ed8153ad027951385e68f17
David Ainslie <dainslie@gmail.com>
2019-05-05 22:25:12 +0100

    Random IP

--- src/main/scala/com/backwards/text/StringOps.scala
+++ src/main/scala/com/backwards/text/StringOps.scala
@@ -52,4 +52,11 @@ trait StringOps {
       case head :: tail => headTransform(head) :: tail.map(tailTransform)
       case list => list
     }) mkString sep
+
+  def randomIp: String = {
+    val random: Int => Int =
+      Random.nextInt
+
+    s"${random(223) + 1}.${(1 to 3).map(_ => random(255)) mkString "."}"
+  }
 }
