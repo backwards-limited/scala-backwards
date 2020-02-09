f84bdf1d1d62c6d2fe37dc9894be0320f70cbfbb
David Ainslie <dainslie@gmail.com>
2020-02-09 16:07:37 +0000

    FS2 Kafka

--- src/it/scala/com/backwards/fs2/FS2KafkaSpec.scala
+++ src/it/scala/com/backwards/fs2/FS2KafkaSpec.scala
@@ -17,9 +17,14 @@ import com.backwards.docker.{DockerCompose, DockerComposeFixture}
 import monocle.macros.syntax.lens._
 import scala.concurrent.duration._
 import monix.eval.Task
-import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, ConsumerRecords, KafkaConsumer}
 import scala.jdk.CollectionConverters._
 
+/**
+ * kafkacat -P -b localhost:9092 -t my-topic
+ *
+ * kafkacat -C -b localhost:9092 -t my-topic -o beginning
+ */
 class FS2KafkaSpec extends AnyWordSpec with Matchers with DockerComposeFixture {
   val dockerCompose: DockerCompose =
     DockerCompose("kafka", Seq(Paths.get("src", "it", "resources", "docker-compose.yml")))
@@ -32,21 +37,6 @@ class FS2KafkaSpec extends AnyWordSpec with Matchers with DockerComposeFixture {
 
       /////////////////////////////////////////////////
 
-
-      val bootstrapServers2Lens: Lens[KafkaConsumerConfig, List[String]] = GenLens[KafkaConsumerConfig](_.bootstrapServers)
-      val groupIdLens: Lens[KafkaConsumerConfig, String] = GenLens[KafkaConsumerConfig](_.groupId)
-      val enableAutoCommitLens: Lens[KafkaConsumerConfig, Boolean] = GenLens[KafkaConsumerConfig](_.enableAutoCommit)
-
-      val y = bootstrapServers2Lens.set(List("localhost:9092")) andThen groupIdLens.set("kafka-tests") andThen enableAutoCommitLens.set(true)
-
-      val consumerTask =
-        KafkaConsumerObservable.createConsumer[String, String](y(KafkaConsumerConfig.default), List("my-topic")).executeOn(io)
-
-      val consumer = Await.result(consumerTask.runToFuture, 60.seconds)
-      consumer.poll(10.seconds.toMillis)
-
-      ////////////////////////////////////////////////
-
       val bootstrapServersLens: Lens[KafkaProducerConfig, List[String]] = GenLens[KafkaProducerConfig](_.bootstrapServers)
       val clientIdLens: Lens[KafkaProducerConfig, String] = GenLens[KafkaProducerConfig](_.clientId)
       val maxInFlightRequestsPerConnectionLens: Lens[KafkaProducerConfig, Int] = GenLens[KafkaProducerConfig](_.maxInFlightRequestsPerConnection)
@@ -58,11 +48,30 @@ class FS2KafkaSpec extends AnyWordSpec with Matchers with DockerComposeFixture {
       val Some(recordMetadata) = producer.send("my-topic", "blah blah").runSyncUnsafe()
       println(recordMetadata)
 
+      /////////////////////////////////////////////////
 
-      ////////////////////
 
-      val records = consumer.poll(10.seconds.toMillis).asScala.map(_.value()).toList
-      println(records)
+      val bootstrapServers2Lens: Lens[KafkaConsumerConfig, List[String]] = GenLens[KafkaConsumerConfig](_.bootstrapServers)
+      val groupIdLens: Lens[KafkaConsumerConfig, String] = GenLens[KafkaConsumerConfig](_.groupId)
+      val enableAutoCommitLens: Lens[KafkaConsumerConfig, Boolean] = GenLens[KafkaConsumerConfig](_.enableAutoCommit)
+      val propertiesLens: Lens[KafkaConsumerConfig, Map[String, String]] = GenLens[KafkaConsumerConfig](_.properties)
+
+      val y = bootstrapServers2Lens.set(List("localhost:9092")) andThen groupIdLens.set("kafka-tests") andThen enableAutoCommitLens.set(true) andThen propertiesLens.modify(_ + (ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest"))
+
+      val v: Task[Vector[String]] =
+        KafkaConsumerObservable.createConsumer[String, String](y(KafkaConsumerConfig.default), List("my-topic"))
+          .map { c =>
+            c.poll(0)
+            c.seekToBeginning(c.assignment())
+            c
+          }
+          .map(_.poll(3000)).map(_.iterator.asScala.toVector.map(_.value))
+
+
+      ////////////////////////////////////////////////
+
+
+      println(v.runSyncUnsafe())
 
     }
   }
