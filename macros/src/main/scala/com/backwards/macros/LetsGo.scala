c69cf43c673f24cf7447ef39d2276a2dd6cc3e52
David Ainslie <dainslie@gmail.com>
2021-05-29 13:49:21 +0100

    Proving ground

---
+++ macros/src/main/scala/com/backwards/macros/LetsGo.scala
@@ -0,0 +1,13 @@
+package com.backwards.macros
+
+import scala.meta._
+
+object LetsGo extends App {
+
+  println("..... made it")
+
+  val program = """object Main extends App { print("Hello!") }"""
+  val tree: Source = program.parse[Source].get
+
+  println(tree.syntax)
+}
