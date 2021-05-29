67ef4a33f57fdfe9904bb2c875f81abed8b670ff
David Ainslie <dainslie@gmail.com>
2021-05-29 13:44:38 +0100

    Proving ground

--- main/src/test/scala/com/backwards/essentialeffects/Ex2.scala
+++ main/src/test/scala/com/backwards/essentialeffects/Ex2.scala
@@ -2,13 +2,14 @@ package com.backwards.essentialeffects
 
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
+import scala.concurrent.duration._
 import cats.effect._
 import cats.implicits._
-import scala.concurrent.duration._
+import com.backwards.essentialeffects.debug._
 
 object TickingClock extends IOApp {
   val tickingClock: IO[Unit] =
-    IO(println(LocalTime.now().truncatedTo(ChronoUnit.SECONDS))) >> IO.sleep(1 second) >> tickingClock
+    IO(println(LocalTime.now().truncatedTo(ChronoUnit.SECONDS))).debug >> IO.sleep(1 second) >> tickingClock
 
   def run(args: List[String]): IO[ExitCode] =
     tickingClock.as(ExitCode.Success)
