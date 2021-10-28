979605be7620f1ce7fa9593ac9693f25d8b0cda0
David Ainslie <dainslie@gmail.com>
2021-10-28 10:54:51 +0100

    StateT

--- main/src/test/scala/com/backwards/fp/monad/MonadTransformerAtoZ.scala
+++ main/src/test/scala/com/backwards/fp/monad/MonadTransformerAtoZ.scala
@@ -89,6 +89,57 @@ object MonadTransformerAtoZ {
   }
 }
 
+object ForExpression extends App {
+  implicit val IOMonad: Monad[IO] =
+    new Monad[IO] {
+      def point[A](a: => A): IO[A] =
+        IO(a)
+
+      def flatMap[A, B](ma: IO[A])(f: A => IO[B]): IO[B] =
+        ma.flatMap(f)
+    }
+
+  final case class IntState(i: Int)
+
+  def add(i: Int): StateT[IO, IntState, Int] =
+    StateT[IO, IntState, Int] { oldState =>
+      val newValue = i + oldState.i
+      val newState = oldState.copy(i = newValue)
+      IO(newState, newValue)
+    }
+
+  def multiply(i: Int): StateT[IO, IntState, Int] =
+    StateT[IO, IntState, Int] { oldState =>
+      val newValue = i * oldState.i
+      val newState = oldState.copy(i = newValue)
+      IO(newState, newValue)
+    }
+
+  val a: StateT[IO, IntState, Int] =
+    add(1)
+
+  val b: IO[(IntState, Int)] =
+    a.run(IntState(1))
+
+  b.map(t => println(s"b state = ${t._1}"))
+  // prints “b state = IntState(2)”
+
+  // A hard-coded example
+  val forExpression: StateT[IO, IntState, Int] =
+    for {
+      _ <- add(2)        // 3
+      _ <- add(3)        // 6
+      x <- multiply(10)  // 6 x 10
+    } yield x
+
+  // “run” the state
+  val result: IO[(IntState, Int)] =
+    forExpression.run(IntState(1))
+
+  // print the final state
+  result.map(tuple => println(s"IntState = ${tuple._1}"))
+}
+
 object LoopWithQuitIncludingDebugging extends IOApp {
   def run(args: List[String]): effect.IO[ExitCode] =
     effect.IO {
