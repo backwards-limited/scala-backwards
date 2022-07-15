f72a1fcc46af3527823aefbb5723192800b16d4f
David Ainslie <dainslie@gmail.com>
2022-07-15 22:09:30 +0100

    Monadic conditions

--- main/src/test/haskell/bookofmonads/Ch4/Ex1Spec.hs
+++ main/src/test/haskell/bookofmonads/Ch4/Ex1Spec.hs
@@ -3,6 +3,7 @@
 -- Utilities for monadic code
 module Ch4.Ex1Spec (spec) where
 
+import Control.Monad.Cont hiding (mapM, sequence, when)
 import Prelude hiding (mapM, sequence)
 import Test.Hspec
 
@@ -62,6 +63,78 @@ takeWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
 dropWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
 firstM     :: Monad m => (a -> m Bool) -> [a] -> m (Maybe a)
 -}
+
+{-
+A note on monadic actions with only side effects e.g.
+put :: s -> State s ()
+
+The following are all equivalent, but the last is preferred:
+
+do
+  () <- put newValue
+
+do
+  _ <- put newValue
+
+do
+  put newValue
+-}
+
+-- Because of unit, Haskell in certain situations, will issue a warning e.g. a warning will be given for:
+example1 :: IO [()]
+example1 = forM ["Alejandro", "Elena"] $ \name -> print ("Hello, " ++ name)
+
+{-
+To avoid any warning, we can use "void" declared as:
+
+void :: Functor m => m a -> m ()
+void = fmap (\_ -> ())
+
+now we can code:
+-}
+example2 :: IO ()
+example2 = void $ forM ["Alejandro", "Elena"]
+                $ \name -> print ("Hello, " ++ name)
+
+{-
+The idiom leads to a lot of "_" equivalent functions e.g.
+
+mapM_ :: Monad m => (a -> m b) -> m a -> m ()
+
+zipWithM_ :: Monad m => (a -> b -> m c) -> [a] -> [b] -> m ()
+
+etc.
+
+One advantage of the empty tuple is the ease of constructing one.
+If you want to create a monadic action m (), you can just use return (), regardless of the monad.
+-}
+
+{-
+In imperative languages, you usually have conditionals where one of the branches may not be present.
+We can provide a similar abstraction for monadic contexts by using the empty tuple as a dummy value:
+-}
+when :: Monad m => Bool -> m () -> m ()
+when cond action = if cond then action else return ()
+
+{-
+Note, the above is already defined in: Control.Monad.Cont.when
+And alternative definition:
+
+when :: Monad m => Bool -> m () -> m ()
+when True  action = action
+when False _      = return ()
+-}
+
+unless :: Monad m => Bool -> m () -> m ()
+unless cond = when (not cond)
+
+{-
+And we can have the condition itself within a monadic context as again defined in Control.Monad.Extra e.g.
+-}
+
+ifM :: Monad m => m Bool -> m a -> m a -> m a
+ifM cond true false = do c <- cond
+                         if c then true else false
 
 {-
 ghci
