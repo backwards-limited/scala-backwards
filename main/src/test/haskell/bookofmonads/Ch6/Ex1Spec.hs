24706bab699f697ac69c12ea75a574dfe1eb13df
David Ainslie <dainslie@gmail.com>
2022-08-14 09:56:10 +0100

    State Monad interface

--- main/src/test/haskell/bookofmonads/Ch6/Ex1Spec.hs
+++ main/src/test/haskell/bookofmonads/Ch6/Ex1Spec.hs
@@ -7,27 +7,52 @@ import Control.Monad.Cont hiding (mapM, sequence, when)
 import Prelude hiding (mapM, sequence)
 import Test.Hspec
 
-newtype State s a = State { runState :: s -> (a, s) }
+newtype State s a = State { run :: s -> (a, s) }
 
 instance Functor (State s) where
-  fmap f (State x) = State (\s -> let (a, s') = x s in (f a, s'))
+  fmap f (State run) = State (\s -> let (a, s') = run s in (f a, s'))
 
 instance Applicative (State s) where
   pure a = State $ \s -> (a, s)
 
-  (<*>) (State f) (State a) =
+  (<*>) (State runf) (State run) =
     State $ \s ->
-      let (f', s') = f s
-          (a', s'') = a s'
-      in (f' a', s'')
+      let (f, s') = runf s
+          (a, s'') = run s'
+      in (f a, s'')
 
 instance Monad (State s) where
   return = pure
 
-  (>>=) (State a) amb =
+  (>>=) (State r) amb =
     State $ \s ->
-      let (a', s') = a s
-      in ($ s') . runState . amb $ a'
+      let (a, s') = r s
+      in ($ s') . run . amb $ a
+
+{-
+DO NOT DO THE FOLLOWING
+
+We could inspect or update the internal state directly.
+For example, this is a function that updates a counter - represented as an integral state — and gives back the current value:
+-}
+nextValue :: State Int Int
+nextValue = State (\i -> (i, i + 1))
+{-
+This is not a good idea for exactly the same reasons that it is not a good idea to access private methods from a module or class.
+Instead, we should use the functions that State provides as an interface to build stateful computations.
+
+The interface for State is comprised of three functions, namely (get, put, modify):
+-}
+get :: State s s
+get = State $ \s -> (s, s)
+
+put :: s -> State s ()
+put s = State $ \_ -> ((), s)
+
+modify :: (s -> s) -> State s ()
+modify f = do
+  s <- get
+  put $ f s
 
 {-
 ghci
--- main/src/test/scala/tech/backwards/bookofmonads/ch6/Ex1Suite.scala
+++ main/src/test/scala/tech/backwards/bookofmonads/ch6/Ex1Suite.scala
@@ -84,4 +84,90 @@ class Ex1Suite extends FunSuite {
 
     println(states.run(5))
   }
+
+  test("State Monad - Interface for our own implementation") {
+    final case class State[S, A](run: S => (S, A))
+
+    def functorState[S]: Functor[State[S, *]] =
+      new Functor[State[S, *]] {
+        def map[A, B](Fa: State[S, A])(f: A => B): State[S, B] =
+          State { s =>
+            val (nextS, a) = Fa.run(s)
+            (nextS, f(a))
+          }
+      }
+
+    def monadState[S]: Monad[State[S, *]] =
+      new Monad[State[S, *]] {
+        def pure[A](a: A): State[S, A] =
+          State(s => (s, a))
+
+        def flatMap[A, B](Fa: State[S, A])(f: A => State[S, B]): State[S, B] =
+          State { s =>
+            val (nextS, a) = Fa.run(s)
+            f(a).run(nextS)
+          }
+
+        def tailRecM[A, B](a: A)(f: A => State[S, A Either B]): State[S, B] =
+          State { s =>
+            f(a).run(s) match {
+              case (nextS, Right(b)) =>
+                (nextS, b)
+              case (nextS, Left(a)) =>
+                tailRecM(a)(f).run(nextS)
+            }
+          }
+      }
+
+    /*
+    DO NOT DO THE FOLLOWING
+
+    We could inspect or update the internal state directly.
+    For example, this is a function that updates a counter - represented as an integral state — and gives back the current value:
+    */
+    val nextValueNotRecommended: State[Int, Int] =
+      State(i => i + 1 -> i)
+
+    val (s, a) = nextValueNotRecommended.run(5)
+    println(s)
+    println(a)
+
+    /*
+    This is not a good idea for exactly the same reasons that it is not a good idea to access private methods from a module or class.
+    Instead, we should use the functions that State provides as an interface to build stateful computations.
+
+    The interface for State is comprised of three functions, namely:
+    */
+    def get[S]: State[S, S] =
+      State(s => s -> s)
+
+    def put[S]: S => State[S, Unit] =
+      s => State(_ => s -> ())
+
+    /*
+    Obvious implementation:
+
+    def modify[S]: (S => S) => State[S, Unit] =
+      f => State(s => f(s) -> ())
+
+    but we can define modify reusing get and put:
+    */
+    def modify[S]: (S => S) => State[S, Unit] =
+      f => monadState.flatMap(get[S])(s => put[S](f(s)))
+
+    // Now we have 2 options to define "nextValue" by reusing our "interface:
+    val nextValue1: State[Int, Int] =
+      monadState.flatMap(get[Int])(s => functorState[Int].map(put[Int](s + 1))(_ => s))
+
+    val (s1, a1) = nextValue1.run(5)
+    println(s1)
+    println(a1)
+
+    val nextValue2: State[Int, Int] =
+      monadState.flatMap(get[Int])(s => functorState[Int].map(modify[Int](_ + 1))(_ => s))
+
+    val (s2, a2) = nextValue2.run(5)
+    println(s2)
+    println(a2)
+  }
 }
