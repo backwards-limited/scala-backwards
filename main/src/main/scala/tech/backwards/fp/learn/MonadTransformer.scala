feb559a2d36a076bccef03d4bbb7df53e6038e4d
David Ainslie <dainslie@gmail.com>
2023-05-03 22:14:25 +0100

    Clean up

--- main/src/main/scala/tech/backwards/fp/learn/MonadTransformer.scala
+++
@@ -1,5 +0,0 @@
-package tech.backwards.fp.learn
-
-abstract class MonadTransformer {
-
-}
--- main/src/test/scala/tech/backwards/fp/learn/MonadTransformerSuite.scala
+++
@@ -1,9 +0,0 @@
-package tech.backwards.fp.learn
-
-import munit.ScalaCheckSuite
-import org.scalacheck.Test
-
-class MonadTransformerSuite extends ScalaCheckSuite {
-  override protected def scalaCheckTestParameters: Test.Parameters =
-    super.scalaCheckTestParameters.withMinSuccessfulTests(100).withMaxDiscardRatio(10)
-}
