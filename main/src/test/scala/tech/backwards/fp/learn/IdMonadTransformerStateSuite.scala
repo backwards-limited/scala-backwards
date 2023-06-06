9ce3deeb602aae4641faaa9074b1776102278613
David Ainslie <dainslie@gmail.com>
2023-06-06 09:26:40 +0100

    IdT State

--- main/src/test/scala/tech/backwards/fp/learn/IdMonadTransformerStateSuite.scala
+++ main/src/test/scala/tech/backwards/fp/learn/IdMonadTransformerStateSuite.scala
@@ -79,255 +79,146 @@ class IdMonadTransformerStateSuite extends ScalaCheckSuite {
     )
   }
 
-  /*property("IdT Monad") {
-    val transformer: IdT[Disjunction[String, *], Int] =
-      Monad[IdT[Disjunction[String, *], *]].pure(10)
+  property("IdT Monad") {
+    val transformer: IdT[State[String, *], Int] =
+      Monad[IdT[State[String, *], *]].pure(10)
 
     assertEquals(
-      Monad[IdT[Disjunction[String, *], *]].flatMap(transformer)(a => Monad[IdT[Disjunction[String, *], *]].pure(a + 1)).value,
-      Right(Id(11))
+      Monad[IdT[State[String, *], *]].flatMap(transformer)(a => Monad[IdT[State[String, *], *]].pure(a + 1)).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      Monad[IdT[Disjunction[String, *], *]].flatMap(Monad[IdT[Disjunction[String, *], *]].pure(10))(a => Monad[IdT[Disjunction[String, *], *]].pure(a + 1)).value,
-      Right(Id(11))
-    )
-
-    assertEquals(
-      Monad[IdT[Disjunction[String, *], *]].flatMap(IdT(Left[String, Id[Int]]("whoops")))(a => Monad[IdT[Disjunction[String, *], *]].pure(a + 1)).value,
-      Left("whoops")
-    )
-
-    assertEquals(
-      Monad[IdT[Disjunction[String, *], *]].flatMap(Monad[IdT[Disjunction[String, *], *]].pure(10))(_ => IdT(Left[String, Id[Int]]("whoops"))).value,
-      Left("whoops")
+      Monad[IdT[State[String, *], *]].flatMap(Monad[IdT[State[String, *], *]].pure(10))(a => Monad[IdT[State[String, *], *]].pure(a + 1)).value.run("foo"),
+      "foo" -> Id(11)
     )
   }
 
   property("IdT Monad syntax") {
-    import tech.backwards.fp.learn.Disjunction.syntax._
     import tech.backwards.fp.learn.Monad.syntax._
 
-    val transformer: IdT[Disjunction[String, *], Int] =
-      10.pure[IdT[Disjunction[String, *], *]]
+    val transformer: IdT[State[String, *], Int] =
+      10.pure[IdT[State[String, *], *]]
 
     assertEquals(
-      transformer.flatMap(a => (a + 1).pure[IdT[Disjunction[String, *], *]]).value,
-      Id(11).right
+      transformer.flatMap(a => (a + 1).pure[IdT[State[String, *], *]]).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      10.pure[IdT[Disjunction[String, *], *]].flatMap(a => (a + 1).pure[IdT[Disjunction[String, *], *]]).value,
-      Id(11).right
+      10.pure[IdT[State[String, *], *]].flatMap(a => (a + 1).pure[IdT[State[String, *], *]]).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      10.pure[IdT[Disjunction[String, *], *]] flatMap (a => IdT(Id(a + 1).right)),
-      IdT(Id(11).right[String])
-    )
-
-    assertEquals(
-      10.pure[IdT[Disjunction[String, *], *]] >>= (a => IdT(Id(a + 1).right)),
-      IdT(Id(11).right[String])
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int]]) >>= (a => IdT(Id(a + 1).right)),
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      IdT(Id(10).right[String]) >>= (_ => IdT("whoops".left[Id[Int]])),
-      IdT("whoops".left[Id[Int]])
+      10.pure[IdT[State[String, *], *]].flatMap(a => IdT(State(_ -> Id(a + 1)))).value.run("foo"),
+      "foo" -> Id(11)
     )
   }
 
   property("IdT Applicative") {
-    val transformerFn: IdT[Disjunction[String, *], Int => Int] =
-      Applicative[IdT[Disjunction[String, *], *]].pure(_ + 1)
+    val transformerFn: IdT[State[String, *], Int => Int] =
+      Applicative[IdT[State[String, *], *]].pure(_ + 1)
 
-    val transformer: IdT[Disjunction[String, *], Int] =
-      Applicative[IdT[Disjunction[String, *], *]].pure(10)
+    val transformer: IdT[State[String, *], Int] =
+      Applicative[IdT[State[String, *], *]].pure(10)
 
     assertEquals(
-      Applicative[IdT[Disjunction[String, *], *]].ap(transformerFn)(transformer).value,
-      Right(Id(11))
+      Applicative[IdT[State[String, *], *]].ap(transformerFn)(transformer).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      Applicative[IdT[Disjunction[String, *], *]].ap(Applicative[IdT[Disjunction[String, *], *]].pure((x: Int) => x + 1))(Applicative[IdT[Disjunction[String, *], *]].pure(10)).value,
-      Right(Id(11))
-    )
-
-    assertEquals(
-      Applicative[IdT[Disjunction[String, *], *]].ap(IdT(Left[String, Id[Int => Int]]("whoops")))(Applicative[IdT[Disjunction[String, *], *]].pure(10)).value,
-      Left("whoops")
-    )
-
-    assertEquals(
-      Applicative[IdT[Disjunction[String, *], *]].ap(Applicative[IdT[Disjunction[String, *], *]].pure((x: Int) => x + 1))(IdT(Left[String, Id[Int]]("whoops"))).value,
-      Left("whoops")
+      Applicative[IdT[State[String, *], *]].ap(Applicative[IdT[State[String, *], *]].pure((x: Int) => x + 1))(Applicative[IdT[State[String, *], *]].pure(10)).value.run("foo"),
+      "foo" -> Id(11)
     )
   }
 
   property("IdT Applicative syntax") {
     import tech.backwards.fp.learn.Applicative.syntax._
-    import tech.backwards.fp.learn.Disjunction.syntax._
 
-    val transformerFn: IdT[Disjunction[String, *], Int => Int] =
-      ((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]]
+    val transformerFn: IdT[State[String, *], Int => Int] =
+      ((x: Int) => x + 1).pure[IdT[State[String, *], *]]
 
-    val transformer: IdT[Disjunction[String, *], Int] =
-      10.pure[IdT[Disjunction[String, *], *]]
+    val transformer: IdT[State[String, *], Int] =
+      10.pure[IdT[State[String, *], *]]
 
     assertEquals(
-      transformerFn.ap(transformer).value,
-      Id(11).right
+      transformerFn.ap(transformer).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      ((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]].ap(10.pure[IdT[Disjunction[String, *], *]]).value,
-      Id(11).right
+      ((x: Int) => x + 1).pure[IdT[State[String, *], *]].ap(10.pure[IdT[State[String, *], *]]).value.run("foo"),
+      "foo" -> Id(11)
     )
 
     assertEquals(
-      ((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]].ap(IdT("whoops".left[Id[Int]])).value,
-      "whoops".left
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int => Int]]).ap(10.pure[IdT[Disjunction[String, *], *]]).value,
-      "whoops".left
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int => Int]]) ap 10.pure[IdT[Disjunction[String, *], *]],
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int => Int]]) <*> 10.pure[IdT[Disjunction[String, *], *]],
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      10.pure[IdT[Disjunction[String, *], *]].ap(((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]]).value,
-      Id(11).right
-    )
-
-    assertEquals(
-      10.pure[IdT[Disjunction[String, *], *]].ap(IdT("whoops".left[Id[Int => Int]])).value,
-      "whoops".left
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int]]).ap(((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]]).value,
-      "whoops".left
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int]]) ap ((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]],
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int]]) <*> ((x: Int) => x + 1).pure[IdT[Disjunction[String, *], *]],
-      IdT("whoops".left[Id[Int]])
+      10.pure[IdT[State[String, *], *]].ap(((x: Int) => x + 1).pure[IdT[State[String, *], *]]).value.run("foo"),
+      "foo" -> Id(11)
     )
   }
 
   property("IdT Functor and Applicative syntax") {
     import tech.backwards.fp.learn.Applicative.syntax._
-    import tech.backwards.fp.learn.Disjunction.syntax._
     import tech.backwards.fp.learn.Functor.syntax._
 
     assertEquals(
-      IdT(Right(Id(10))) `<$>` (x => (y: Int) => x + y) <*> IdT(Right(Id(1))),
-      IdT(Right(Id(11)))
-    )
-
-    assertEquals(
-      IdT("whoops".left[Id[Int]]) `<$>` (x => (y: Int) => x + y) <*> IdT(Id(1).right),
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      IdT(Id(10).right[String]) `<$>` (x => (y: Int) => x + y) <*> IdT("whoops".left[Id[Int]]),
-      IdT("whoops".left[Id[Int]])
+      (IdT(State((s: String) => s"${s}bar" -> Id(10))) `<$>` (x => (y: Int) => x + y) <*> IdT(State((s: String) => s"${s}baz" -> Id(1)))).value.run("foo"),
+      "foobarbaz" -> Id(11)
     )
 
     val add: Int => Int => Int =
       x => y => x + y
 
     assertEquals(
-      IdT(Id(10).right[String]) `<$>` add <*> IdT("whoops".left[Id[Int]]),
-      IdT("whoops".left[Id[Int]])
+      (IdT(State((s: String) => s"${s}bar" -> Id(10))) `<$>` add <*> IdT(State((s: String) => s"${s}baz" -> Id(1)))).value.run("foo"),
+      "foobarbaz" -> Id(11)
     )
   }
 
   property("IdT Functor and Applicative function syntax") {
     import tech.backwards.fp.learn.Applicative.syntax._
-    import tech.backwards.fp.learn.Disjunction.syntax._
     import tech.backwards.fp.learn.Functor.syntax.function._
 
     assertEquals(
-      ((x: Int) => (y: Int) => x + y) `<$>` IdT(Right(Id(10))) <*> IdT(Right(Id(1))),
-      IdT(Right(Id(11)))
-    )
-
-    assertEquals(
-      ((x: Int) => (y: Int) => x + y) `<$>` IdT("whoops".left[Id[Int]]) <*> IdT(Right(Id(1))),
-      IdT("whoops".left[Id[Int]])
-    )
-
-    assertEquals(
-      ((x: Int) => (y: Int) => x + y) `<$>` IdT(Id(10).right[String]) <*> IdT("whoops".left[Id[Int]]),
-      IdT("whoops".left[Id[Int]])
+      (((x: Int) => (y: Int) => x + y) `<$>` IdT(State((s: String) => s"${s}bar" -> Id(10))) <*> IdT(State((s: String) => s"${s}baz" -> Id(1)))).value.run("foo"),
+      "foobarbaz" -> Id(11)
     )
 
     val add: Int => Int => Int =
       x => y => x + y
 
     assertEquals(
-      add `<$>` IdT(Right(Id(10))) <*> IdT(Right(Id(1))),
-      IdT(Right(Id(11)))
+      (add `<$>` IdT(State((s: String) => s"${s}bar" -> Id(10))) <*> IdT(State((s: String) => s"${s}baz" -> Id(1)))).value.run("foo"),
+      "foobarbaz" -> Id(11)
     )
   }
 
   property("IdT for comprehension") {
-    import tech.backwards.fp.learn.Disjunction.syntax._
     import tech.backwards.fp.learn.Functor.syntax._
     import tech.backwards.fp.learn.Monad.syntax._
 
-    val transformer: IdT[String Disjunction *, Int] =
+    val transformer: IdT[State[String, *], Int] =
       for {
-        x <- 10.pure[IdT[String Disjunction *, *]]
-        y <- 11.pure[IdT[String Disjunction *, *]]
-        z <- 12.pure[IdT[String Disjunction *, *]]
+        x <- 10.pure[IdT[State[String, *], *]]
+        y <- 11.pure[IdT[State[String, *], *]]
+        z <- 12.pure[IdT[State[String, *], *]]
       } yield x + y + z
 
     assertEquals(
-      transformer.value,
-      Right(Id(33))
+      transformer.value.run("foo"),
+      "foo" -> Id(33)
     )
 
     assertEquals(
-      for {
-        x <- IdT.lift(10.right[String])
-        y <- IdT.lift(11.right[String])
-        z <- IdT.lift(12.right[String])
-        _ <- IdT(0.right[String].map(Id.apply)) // Without "lift"
-      } yield x + y + z,
-      IdT(Id(33).right[String])
+      (for {
+        x <- IdT.lift(State((s: String) => s"${s}bar" -> 10))
+        y <- IdT.lift(State((s: String) => s"${s}baz" -> 11))
+        z <- IdT.lift(State((s: String) => s"$s-and-then-some" -> 12))
+        _ <- IdT(State((s: String) => s"$s-without-lift" -> Id(0))) // Without "lift"
+      } yield x + y + z).value.run("foo"),
+      "foobarbaz-and-then-some-without-lift" -> Id(33)
     )
-
-    assertEquals(
-      for {
-        x <- IdT.lift(10.right[String])
-        y <- IdT.lift("whoops".left[Int])
-        z <- IdT.lift(12.right[String])
-      } yield x + y + z,
-      IdT("whoops".left[Id[Int]])
-    )
-  }*/
+  }
 }
